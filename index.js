6190c62da35af33215e79df5a497a821ceb8ef8a
thepunkprogrammer <mkodekar1990@gmail.com>
2017-10-12 18:14:34 +0530

    The get request

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", function(req, res) {
+    console.log("GET REQUEST: ");
+    res.send({name: "Rehan"});
+});
+
+
 app.listen(process.env.port || '4000',  function() {
     console.log("listeninig to requests");
 });
